6360b044681087f2bc457f7252440b5c356ffc61
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-16 18:44:48 +0200

    reworked add project button style

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,13 +1,18 @@
 @extends('admin.layouts.base')
 
 @section('contents')
-    <div class="d-flex align-items-center">
-        <div class="icon_plus">
-            <a href="{{ route('admin.projects.create') }}">
-                <i class="fa-sharp fa-solid fa-plus"></i>
-            </a>
+    <div class="d-flex flex-row justify-content-between">
+        <div class="d-flex align-items-center">
+            <h1 class="page-title">PROJECTS</h1>
         </div>
-        <h1 class="page-title">PROJECTS</h1>
+{{--            <h4 class="d-flex align-items-center m-0">Add project</h4>--}}
+            <div class="d-flex justify-content-center align-items-center" type="button" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Add new project">
+                <div class="ms-3 icon_plus">
+                    <a href="{{ route('admin.projects.create') }}">
+                        <i class="fa-sharp fa-solid fa-plus"></i>
+                    </a>
+                </div>
+            </div>
     </div>
 {{--    @php $project = session('delete_success') @endphp   --}}
     @if (session('delete_success'))
