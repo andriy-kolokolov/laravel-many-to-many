b2ce3cc2a4c68409dd13bd489a5e669ba4dc52f0
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-16 23:29:32 +0200

    reworked styles

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="shadow-lg admin-footer" id="footer">
     <div class="container-fluid h-100 d-flex justify-content-center align-items-center">
         <div class="footer-icons">
-            <i class="fa-brands fa-github"></i>
-            <i class="fa-brands fa-linkedin"></i>
+            <a href="https://github.com/andriy-kolokolov"><i class="fa-brands fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/andriy-kolokolov-a527b2167/"><i class="fa-brands fa-linkedin"></i></a>
         </div>
     </div>
 </footer>
